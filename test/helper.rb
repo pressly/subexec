af0854e407f4534b00ff4879a7d22594d7f3161a
Hampton Catlin <hcatlin@gmail.com>
2011-02-28 14:30:25 +0000

    We don't always *literally* want to run a test if we load up the same environment. So, splitting these up. This is needed for making Benchmark its own thing and not part of the test suite.

--- test/helper.rb
+++ test/helper.rb
@@ -6,15 +6,7 @@ begin
 rescue LoadError
 end
 
-ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-TEST_ROOT = File.join(ROOT, 'test')
-
-$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$:.unshift(File.dirname(__FILE__))
-require 'subexec'
-
-# Test program
-TEST_PROG = File.join(TEST_ROOT, 'helloworld.sh')
+require File.join(File.dirname(__FILE__), 'loader')
 
 #
 class Test::Unit::TestCase
