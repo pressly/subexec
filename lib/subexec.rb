1d36d0c0d80aa7b0507622c84d59e8b41865c809
interu <beautybeast07@gmail.com>
2013-04-01 17:58:55 +0900

    Fix: Written to the pipe exceed 64Kb, at which point the command never finishes.

--- lib/subexec.rb
+++ lib/subexec.rb
@@ -1,28 +1,29 @@
+# #-*- encoding : utf-8 -*-
 # # Subexec
 # * by Peter Kieltyka
 # * http://github/nulayer/subexec
-# 
+#
 # ## Description
-# 
+#
 # Subexec is a simple library that spawns an external command with
 # an optional timeout parameter. It relies on Ruby 1.9's Process.spawn
 # method. Also, it works with synchronous and asynchronous code.
-# 
+#
 # Useful for libraries that are Ruby wrappers for CLI's. For example,
 # resizing images with ImageMagick's mogrify command sometimes stalls
 # and never returns control back to the original process. Subexec
 # executes mogrify and preempts if it gets lost.
-# 
+#
 # ## Usage
-# 
+#
 # # Print hello
 # sub = Subexec.run "echo 'hello' && sleep 3", :timeout => 5
 # puts sub.output     # returns: hello
 # puts sub.exitstatus # returns: 0
-# 
+#
 # # Timeout process after a second
 # sub = Subexec.run "echo 'hello' && sleep 3", :timeout => 1
-# puts sub.output     # returns: 
+# puts sub.output     # returns:
 # puts sub.exitstatus # returns:
 
 class Subexec
@@ -41,7 +42,7 @@ class Subexec
     sub.run!
     sub
   end
-  
+
   def initialize(command, options={})
     self.command    = command
     self.lang       = options[:lang]      || "C"
@@ -49,7 +50,7 @@ class Subexec
     self.log_file   = options[:log_file]
     self.exitstatus = 0
   end
-  
+
   def run!
     if RUBY_VERSION >= '1.9' && RUBY_ENGINE != 'jruby'
       spawn
@@ -60,18 +61,21 @@ class Subexec
 
 
   private
-  
+
     def spawn
       # TODO: weak implementation for log_file support.
       # Ideally, the data would be piped through to both descriptors
-      r, w = IO.pipe      
+      r, w = IO.pipe
       if !log_file.nil?
         self.pid = Process.spawn({'LANG' => self.lang}, command, [:out, :err] => [log_file, 'a'])
       else
         self.pid = Process.spawn({'LANG' => self.lang}, command, STDERR=>w, STDOUT=>w)
       end
       w.close
-      
+
+      # Ref: http://stackoverflow.com/questions/13829830/ruby-process-spawn-stdout-pipe-buffer-size-limit
+      lines = r.readlines
+
       @timer = Time.now + timeout
       timed_out = false
 
@@ -106,13 +110,13 @@ class Subexec
       else
         # The subprocess exited on its own
         self.exitstatus = $?.exitstatus
-        self.output = r.readlines.join("")
+        self.output = (lines + r.readlines).join("")
       end
       r.close
-      
+
       self
     end
-  
+
     def exec
       if !(RUBY_PLATFORM =~ /win32|mswin|mingw/).nil?
         self.output = `set LANG=#{lang} && #{command} 2>&1`
